aa1aca68ff10e253fcee0278034cf33cad09842f
KSS-Git <1semyon@gmail.com>
2021-04-03 13:34:44 +0300

    Lesson_stage

---
+++ Homework_2_Magnit.py
@@ -0,0 +1,61 @@
+import requests
+from urllib.parse import urljoin
+import bs4
+import pymongo
+import time
+
+
+class MagnitParse:
+    def __init__(self, start_url, mongo_url):
+        self.start_url = start_url
+        client = pymongo.MongoClient(mongo_url)
+        self.db = client["gb_parse_19_03_21"]
+
+    def _get_response(self, url, *args, **kwargs):
+        for _ in range(5):
+            response = requests.get(url, *args, **kwargs)
+            if response.status_code == 200:
+                return response
+            time.sleep(0.5)
+        raise ValueError("URL DIE")
+
+    def get_soup(self, url, *args, **kwargs) -> bs4.BeautifulSoup:
+        soup = bs4.BeautifulSoup(self._get_response(url, *args, **kwargs).text, "lxml")
+        return soup
+
+    @property
+    def _template(self):
+        data_template = {
+            "product_name": lambda a: a.find("div", attrs={"class": "card-sale__title"}).text,
+            "url": lambda a: urljoin(self.start_url, a.attrs.get("href", "/")),
+            "image_url": lambda a: urljoin(
+                self.start_url, a.find("picture").find("img").attrs.get("data-src", "/")
+            ),
+        }
+        return data_template
+
+    def run(self):
+        for product in self._parse(self.get_soup(self.start_url)):
+            self.save(product)
+
+    def _parse(self, soup):
+        products_a = soup.find_all("a", attrs={"class": "card-sale"})
+        for prod_tag in products_a:
+            product_data = {}
+            for key, func in self._template.items():
+                try:
+                    product_data[key] = func(prod_tag)
+                except AttributeError:
+                    pass
+            yield product_data
+
+    def save(self, data):
+        collection = self.db["magnit"]
+        collection.insert_one(data)
+
+
+if __name__ == "__main__":
+    url = "https://magnit.ru/promo/"
+    mongo_url = "mongodb://localhost:27017"
+    parser = MagnitParse(url, mongo_url)
+    parser.run()
